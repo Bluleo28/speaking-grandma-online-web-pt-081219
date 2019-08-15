cf8f84dcafa987dc35345d862d62a3c1a1a1ef1f
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-15 01:24:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def  speak_to_grandma
 if 'I love you grandma!'
  puts 'HUH?! SPEAK UP, SONNY!.'
 else if 'I love you grandma'
-puts 'NO, NOT SINCE 1938!'
+puts 'WHAT?' 'NO, NOT SINCE 1938!'
 else if 'I LOVE YOU GRANDMA!'
 else 'I LOVE YOU TOO PUMPKIN!'
 end
