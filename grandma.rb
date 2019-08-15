5f4c98d7aafc6d1da534590c6e3069f837ac3336
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-15 01:22:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,8 @@ if 'I love you grandma!'.downcase
  puts 'HUH?! SPEAK UP, SONNY!.'
 else if 'I love you grandma'. upcase
 puts 'NO, NOT SINCE 1938!'
-else if 'I LOVE YOU GRANDMA!'.capatalize
-  
+else if 'I LOVE YOU GRANDMA!'
+else 'I LOVE YOU TOO PUMPKIN!'
 end
 end
  
