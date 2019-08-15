04015c932f961338ef18a40ecec0873f2c5525f8
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-15 01:18:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 def  speak_to_grandma 
 if 'I love you grandma!'.downcase
-  return
-  
-  
+ puts 'HUH?! SPEAK UP, SONNY!.'
+else if 'I love you grandma'. upcase
+puts 'NO, NOT SINCE 1938!'
+
+ 
   
