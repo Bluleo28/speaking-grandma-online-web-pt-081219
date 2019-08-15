e243fa8c3c415d4c15cf24a4fa2f16da018cdbf3
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-15 01:45:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def  speak_to_grandma(phrase) 
  if phrase ==  "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN"
- elseif phrase == phrase.upcase
+ elseif phrase == "WHAT DID YOU EAT TODAY?"
    return "NO, NOT SINCE 1938!"
 else
  return "HUH?! SPEAK UP, SONNY!"
